c4a540e78c8c21c45ee8ddbf1410676d27c5c8fb
王健 <wangjian020426@126.com>
2026-02-27 17:31:27 +0800

    #code#001 #编写redis工具类，初始化发送接口，用户查询接口

--- src/main/java/com/wj/form/wall/controller/UserController.java
+++ src/main/java/com/wj/form/wall/controller/UserController.java
@@ -2,6 +2,7 @@ package com.wj.form.wall.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * packageName com.wj.form.wall.controller
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @description 用户信息操作接口
  */
 @RequestMapping("/user")
+@RestController
 public class UserController {
 
 }
