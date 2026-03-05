cb580bf65435f9dd6f1b22222f4751c709fa311f
王健 <wangjian020426@126.com>
2026-03-05 13:08:55 +0800

    #code#008 #修改prod配置文件

--- src/main/java/com/wj/future/compus/controller/ArticleController.java
+++ src/main/java/com/wj/future/compus/controller/ArticleController.java
@@ -1,10 +1,12 @@
 package com.wj.future.compus.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wj.future.compus.annotation.AuthIsLogin;
+import com.wj.future.compus.entity.es.po.ArticleEsPojo;
 import com.wj.future.compus.entity.pojo.ArticlePojo;
 import com.wj.future.compus.entity.pojo.UserPojo;
 import com.wj.future.compus.entity.request.ArticleRequest;
@@ -16,10 +18,23 @@ import com.wj.future.compus.service.ArticleService;
 import com.wj.future.compus.util.UserUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.Query;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/article")
@@ -30,6 +45,9 @@ public class ArticleController {
 
     @Autowired
     private ArticleService articleService;
+
+    @Autowired
+    private ElasticsearchOperations elasticsearchOperations;
 
     @AuthIsLogin
     @PostMapping("/publish")
@@ -87,9 +105,37 @@ public class ArticleController {
             }
         }
 
-        // 查询分页列表
-        Page<ArticleResponse> articlePojoPage = articleService.pageArticleList(articleRequest);
+        // todo:使用es查询
+        Criteria criteria = new Criteria();
 
-        return R.ok(articlePojoPage);
+        criteria.and(new Criteria("title")).contains(articleRequest.getQuery());
+        criteria.and(new Criteria("content")).contains(articleRequest.getQuery()); // 姓名包含"张"
+        criteria.and(new Criteria("schoolName").is(articleRequest.getSchoolName()));
+        criteria.and(new Criteria("schoolId").is(articleRequest.getSchoolId()));
+        criteria.and(new Criteria("viewRange").is(articleRequest.getViewRange()));
+
+        Sort likeCountSort = Sort.by(Sort.Direction.DESC, "likeCount");
+        Sort heatSort = Sort.by(Sort.Direction.DESC, "heat");
+        Sort createTimeSort = Sort.by(Sort.Direction.DESC, "createTime");
+
+        // 步骤2：构建Query对象
+        Query query = new CriteriaQuery(criteria);
+        query.addSort(createTimeSort);
+        query.addSort(likeCountSort);
+        query.addSort(heatSort);
+        // 分页：第0页，每页10条
+        query.setPageable(PageRequest.of(articleRequest.getPageNum(),articleRequest.getPageSize()));
+        SearchHits<ArticleEsPojo> esPojoSearchHits = elasticsearchOperations.search(query, ArticleEsPojo.class);
+
+        List<ArticleResponse> articleResponses = null;
+        if (esPojoSearchHits.getTotalHits() > 0){
+            List<ArticleEsPojo> articleEsPojoList= esPojoSearchHits.stream().map(SearchHit::getContent).collect(Collectors.toList());
+            articleResponses = BeanUtil.copyToList(articleEsPojoList, ArticleResponse.class);
+
+        }
+
+        Page<ArticleResponse> articleResponsePage = new Page<>(articleRequest.getPageNum(),articleRequest.getPageSize(),esPojoSearchHits.getTotalHits());
+        articleResponsePage.setRecords(articleResponses);
+        return R.ok(articleResponsePage);
     }
 }
