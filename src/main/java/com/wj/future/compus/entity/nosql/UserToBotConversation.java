e829fc22c1f8273a9faa644d8021332e7bc414f0
王健 <wangjian020426@126.com>
2026-03-11 09:51:08 +0800

    #code#008 #修改文件位置

--- src/main/java/com/wj/future/compus/entity/nosql/UserToBotConversation.java
+++ src/main/java/com/wj/future/compus/entity/nosql/UserToBotConversation.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
-public class UserToBotConversation {
+public class userToBotConversation {
     @Schema(description = "ai回复的内容")
     private String bot;
 
